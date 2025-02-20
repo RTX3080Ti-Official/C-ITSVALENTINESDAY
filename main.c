3e5f3fa9b6cb5f464153d0a72420aacbd523faa4
RTX3080Ti <rtx3080ti0415@gmail.com>
2025-02-20 17:12:41 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+ 
+int main() { 
+    // Code made by RTX3080Ti-Official on Github. You can remove this line.
+    printf("  **      **  \n"); 
+    printf(" *  *    *  * \n"); 
+    printf("*    ****    *\n"); 
+    printf("*             *\n"); 
+    printf(" *           * \n"); 
+    printf("  *         *  \n"); 
+    printf("   *       *   \n"); 
+    printf("    *     *    \n"); 
+    printf("     *   *     \n"); 
+    printf("      * *      \n"); 
+    printf("       *       \n"); 
+     
+    return 0; 
+} 
